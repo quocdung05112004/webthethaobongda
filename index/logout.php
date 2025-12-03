bf6d36b75cf15e10838f311cae259bdb496ccab4
yeuembatchap <doideplai@gmail.com>
2025-12-03 22:43:42 +0700

    Update profile page + login

--- index/logout.php
+++ index/logout.php
@@ -4,6 +4,6 @@ if (session_status() == PHP_SESSION_NONE) session_start();
 // Hủy session và quay về trang đăng nhập
 session_unset();
 session_destroy();
-header('Location: login.php');
+header('Location: index.php');
 exit;
 ?>
